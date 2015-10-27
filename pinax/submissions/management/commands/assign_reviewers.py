dce6b43fa220027f764f39015fdd30fbe318316f
Patrick Altman <paltman@gmail.com>
2015-10-26 21:32:15 -0500

    Port management commands from Symposion

---
+++ pinax/submissions/management/commands/assign_reviewers.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand
+
+from ..models import ReviewAssignment, SubmissionBase
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        for submission in SubmissionBase.objects.filter(cancelled=False):
+            print "Creating assignments for {}".format(submission)
+            ReviewAssignment.create_assignments(submission)
---
+++ pinax/submissions/management/commands/create_review_permissions.py
@@ -0,0 +1,21 @@
+from django.core.management.base import BaseCommand
+
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        ct, created = ContentType.objects.get_or_create(
+            model="",
+            app_label="reviews",
+            defaults={"name": "reviews"}
+        )
+        for action in ["review", "manage"]:
+            perm, created = Permission.objects.get_or_create(
+                codename="can_{}".format(action),
+                content_type__pk=ct.id,
+                defaults={"name": "Can {}".format(action), "content_type": ct}
+            )
+            print perm
