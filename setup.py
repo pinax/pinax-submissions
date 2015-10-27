4531611c25d974e63d0310e0da18a49091a80563
Patrick Altman <paltman@gmail.com>
2015-10-26 21:46:42 -0500

    0.2.1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     description="",
     name="pinax-submissions",
     long_description=read("README.rst"),
-    version="0.2",
+    version="0.2.1",
     url="http://github.com/pinax/pinax-submissions/",
     license="MIT",
     packages=find_packages(),
