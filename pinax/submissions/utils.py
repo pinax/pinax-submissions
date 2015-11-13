9443567108774d85b664badb0d70759c28cac572
Greg Newman <greg@20seven.org>
2015-11-13 15:29:01 -0500

    Login mixin

--- pinax/submissions/utils.py
+++ pinax/submissions/utils.py
@@ -1,6 +1,21 @@
 import os
 import uuid
 
+from django.views import generic
+
+
+class LoggedInMixin(object):
+    """
+    A mixin requiring a user to be logged in.
+
+    """
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated():
+            raise http.Http404
+        self.kwargs = kwargs
+        return super(LoggedInMixin, self).dispatch(request, *args, **kwargs)
+
 
 def uuid_filename(instance, filename):
     ext = filename.split(".")[-1]
