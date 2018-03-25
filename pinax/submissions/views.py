20c5e62e3d8997bc3f3dce6ce480167e5d90b190
Katherine Michel <kthrnmichel@gmail.com>
2018-03-25 11:34:23 -0500

    Removing compat.py and updating views.py login_required

--- pinax/submissions/views.py
+++ pinax/submissions/views.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 from django.contrib import messages
 from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
 from django.core.mail import send_mass_mail
 from django.db.models import Q
 from django.http import (
@@ -23,8 +24,6 @@ from django.views.generic import (
     ListView,
     UpdateView,
 )
-
-from .compat import login_required
 
 from .conf import settings
 from .forms import (
