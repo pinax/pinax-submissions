2effcb31c11bdfd2e2f778a19e380bb57ee176f0
KatherineMichel <kthrnmichel@gmail.com>
2020-05-21 11:40:30 -0500

    Remove "r"

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys
 from setuptools import find_packages, setup
 
 VERSION = "2.0.0"
-LONG_DESCRIPTION = r"""
+LONG_DESCRIPTION = """
 .. image:: http://pinaxproject.com/pinax-design/patches/pinax-submissions.svg
     :target: https://pypi.python.org/pypi/pinax-submissions/
 
